e8774c73b458513cd0dc595919cccf558c890841
unknown <alvin.ictn@gmail.com>
2020-12-20 23:58:43 +0700

    add statusnavbar

---
+++ src/components/statusNavbar/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Card} from 'react-bootstrap'
+
+export default function StatusNavbar() {
+    return (
+        <Card className="card d-flex mx-5 h-50">
+           <Card.Body>
+               <Card.Text>
+                   DD
+               </Card.Text>
+           </Card.Body>
+        </Card>
+    )
+}
