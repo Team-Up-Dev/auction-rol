98c0ded3faac2cee2b012587c176def25c3b62de
unknown <alvin.ictn@gmail.com>
2020-12-21 00:47:25 +0700

    add currencies @alvin-ictn

--- src/components/statusNavbar/index.jsx
+++ src/components/statusNavbar/index.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import styles from "./statusNavbar.module.scss";
+import zeny from "../../assets/currencies/zeny.png";
+import dust from "../../assets/currencies/dust.png";
 
 export default function StatusNavbar() {
-    return (
-        <Card className="card d-flex mx-5 h-50">
-           <Card.Body>
-               <Card.Text>
-                   DD
-               </Card.Text>
-           </Card.Body>
-        </Card>
-    )
+  const currencies = {
+    zeny: [],
+    dust: [],
+  };
+
+  return (
+    <Card className={`d-flex mx-5 mb-5 h-50 ${styles.navbarContainer}`}>
+      <Card.Body className="d-flex justify-content-around">
+        <div className="d-flex align-items-center">
+          <img className={`${styles.currencies} mx-2`} src={zeny} alt="" />
+          <span className={`font-weight-bold`}>DD</span>
+        </div>
+        <div className="d-flex align-items-center">
+          <img className={`${styles.currencies} mx-2`} src={dust} alt="" />
+          <span className={`font-weight-bold`}>DD</span>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 }
