d9ed738b76b6b4a1b31cb80ce2f04794fc97d756
unknown <alvin.ictn@gmail.com>
2020-12-21 15:09:42 +0700

    add dummy datas

--- src/components/rightSidebar/index.jsx
+++ src/components/rightSidebar/index.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import styles from './rightSidebar.module.scss';
+import React from "react";
+import styles from "./rightSidebar.module.scss";
+import NumberFormat from "react-number-format";
 
 export default function RightSidebar() {
-    return (
-        <div className={`${styles.rightSidebarContainer} card`}>
-            ddasdasdasdas
-        </div>
-    )
+  const materials = [
+    { nick: "Main Character", amethyst: 1000, emerald: 1000, ruby:1000, sapphire: 1000, sardonyx: 1000, topaz: 1000 },
+    { nick: "Main Character", amethyst: 1000, emerald: 1000, ruby:1000, sapphire: 1000, sardonyx: 1000, topaz: 1000 },
+    { nick: "Main Character", amethyst: 1000, emerald: 1000, ruby:1000, sapphire: 1000, sardonyx: 1000, topaz: 1000 },
+    { nick: "Archer 60", amethyst: 1000, emerald: 1000, ruby:1000, sapphire: 1000, sardonyx: 1000, topaz: 1000 },
+  ];
+  return (
+    <div className={`${styles.rightSidebarContainer} card`}>
+      <div className={`${styles.rightSidebarContent}`}>
+
+      </div>
+    </div>
+  );
 }
