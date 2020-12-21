d8b42f9afbb805a47916ac748d43314252750a44
unknown <alvin.ictn@gmail.com>
2020-12-21 15:42:25 +0700

    add materials page

--- src/components/rightSidebar/index.jsx
+++ src/components/rightSidebar/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./rightSidebar.module.scss";
 import NumberFormat from "react-number-format";
+import { FaAngleRight } from "react-icons/fa";
+import { Card, OverlayTrigger, Popover } from "react-bootstrap";
 
 export default function RightSidebar() {
   const materials = [
@@ -41,7 +43,17 @@ export default function RightSidebar() {
       topaz: 1000,
     },
   ];
-
+  let amethyst = 33;
+  const materialsName = [
+    "Amethyst",
+    "Emerald",
+    "Ruby",
+    "Sapphire",
+    "Sardonyx",
+    "Topaz",
+  ];
+  console.log(materials);
+  console.log(eval(`materials[0].${materialsName[0].toLocaleLowerCase()}`));
   const totalAmethyst =
     materials.length > 2
       ? materials.map((item) => item.amethyst).reduce((a, b) => a + b)
@@ -62,7 +74,7 @@ export default function RightSidebar() {
       ? materials.map((item) => item.sapphire).reduce((a, b) => a + b)
       : materials.sapphire[0];
 
-  const totalsardonyx =
+  const totalSardonyx =
     materials.length > 2
       ? materials.map((item) => item.sardonyx).reduce((a, b) => a + b)
       : materials.sardonyx[0];
@@ -74,7 +86,64 @@ export default function RightSidebar() {
 
   return (
     <div className={`${styles.rightSidebarContainer} card`}>
-      <div className={`${styles.rightSidebarContent}`}></div>
+      <div className={`${styles.rightSidebarContent}`}>
+        {materialsName.map((material) => (
+          <div className="d-flex align-items-center">
+            <OverlayTrigger
+              placement="bottom"
+              delay={{ show: 0, hide: 200 }}
+              overlay={
+                <Popover id="popover-basic">
+                  <Popover.Title as="h3">
+                    <NumberFormat
+                      value={eval(`total${material}`)}
+                      displayType={"text"}
+                      thousandSeparator={"."}
+                      decimalSeparator={","}
+                      prefix={""}
+                      renderText={(value) => <div>{value}</div>}
+                    />
+                  </Popover.Title>
+                  <Popover.Content>
+                    {materials.map((item, idx) => (
+                      <span className="d-flex align-items-center" key={idx}>
+                        {idx + 1}
+                        <FaAngleRight />
+                        <NumberFormat
+                          value={eval(`item.${material.toLowerCase()}`)}
+                          displayType={"text"}
+                          thousandSeparator={"."}
+                          decimalSeparator={","}
+                          prefix={""}
+                          renderText={(value) => <div>{value}</div>}
+                        />
+                      </span>
+                    ))}
+                  </Popover.Content>
+                </Popover>
+              }
+            >
+              <div className="d-flex align-items-center">
+                <img
+                  className={`${styles.materialsImage} mx-2`}
+                  src={`/images/items/materials/${material.toLowerCase()}-clean.png`}
+                  alt=""
+                />
+                <span className={`font-weight-bold`}>
+                  <NumberFormat
+                    value={eval(`total${material}`)}
+                    displayType={"text"}
+                    thousandSeparator={"."}
+                    decimalSeparator={","}
+                    prefix={""}
+                    renderText={(value) => <div>{value}</div>}
+                  />
+                </span>
+              </div>
+            </OverlayTrigger>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
