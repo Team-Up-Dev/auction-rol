d6774d1b6bf8c2ad640539b765e6f03b587497e0
unknown <alvin.ictn@gmail.com>
2020-12-21 17:29:02 +0700

    add new dummy data @alvin-ictn

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Item from "./components/item";
 import { Container, Card } from "react-bootstrap";
 import FormPost from "./components/form";
-import StatusNavbar from './components/statusNavbar'
-import RightSidebar from './components/rightSidebar'
+import StatusNavbar from "./components/statusNavbar";
+import RightSidebar from "./components/rightSidebar";
 
 const dummyData = [
   {
-    type:"cards",
-    category:"longsword",
-    level:61,
-    title:"Eclipse Card",
-    description:"Card that can be Equiped on specific part of a Gear",
-    option1: 200,
-    option2: 10,
-    option3: 23,
-    option4: 111,
-    maxOption1: 547,
-    maxOption2: 15,
-    maxOption3: 27,
-    maxOption4: 192,
-    optionName1: "M.ATK",
-    optionName2: "ATK",
-    optionName3: "AGI",
-    optionName4: "STR",
-  },
-  {
-    type:"cards",
-    category:"longsword",
-    level:29,
-    title:"Wolf Card",
-    description:"Card that can be Equiped on specific part of a Gear",
+    type: "cards",
+    category: "longsword",
+    level: 29,
+    title: "Wolf Card",
+    description: "Card that can be Equiped on specific part of a Gear",
     option1: 191,
     option2: "3.5%",
     option3: "3.3%",
@@ -41,14 +22,14 @@ const dummyData = [
     optionName1: "ATK",
     optionName2: "ATK",
     optionName3: "Critical Dmg Inc.",
-    qc: 56
+    qc: 56,
   },
   {
-    type:"cards",
-    category:"longsword",
-    level:20,
-    title:"Wolf Card",
-    description:"Card that can be Equiped on specific part of a Gear",
+    type: "cards",
+    category: "longsword",
+    level: 20,
+    title: "Wolf Card",
+    description: "Card that can be Equiped on specific part of a Gear",
     option1: 112,
     option2: "2.25%",
     option3: "3.6%",
@@ -58,23 +39,39 @@ const dummyData = [
     optionName1: "ATK",
     optionName2: "ATK",
     optionName3: "Critical Dmg Inc.",
-    qc: 41
-  }
+    qc: 41,
+  },
+  {
+    type: "cards",
+    category: "longsword",
+    level: 60,
+    title: "Soldier Skel Card",
+    description: "Card that can be Equiped on specific part of a Gear",
+    option1: 448,
+    option2: "12.25%",
+    maxOption1: 825,
+    maxOption2: "15.4%",
+    optionName1: "ATK",
+    optionName2: "Critical ATK Rate.",
+    qc: 47,
+  },
 ];
 
 function App() {
   return (
     <div className="App">
-      <StatusNavbar/>
+      <StatusNavbar />
       <Container>
         <div md={3}></div>
         <div md={8}>
           <Card className="p-1 d-flex flex-row flex-wrap justify-content-center">
-            {dummyData.map(item => <Item {...item}/>)}
+            {dummyData.map((item) => (
+              <Item {...item} />
+            ))}
           </Card>
-          <FormPost />
+          {/* <FormPost /> */}
         </div>
-        <RightSidebar/>
+        <RightSidebar />
       </Container>
     </div>
   );
